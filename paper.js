8ad1ffc7f559e4c5ce6244be5e3a931a4fe98415
SRISHTINIGAM02 <72446091+SRISHTINIGAM02@users.noreply.github.com>
2020-12-11 22:01:11 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,11 @@
+class paper extends BaseClass
+{
+    constructor(x,y,r){
+    super(x,y,r)
+    this.image=loadImage("paper.png");
+    }
+    display(){
+       
+        super.display();
+    }
+}
